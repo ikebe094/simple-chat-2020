a8034ad917910aab8ce77bb250867be35515766e
ikebe094 <53401298+ikebe094@users.noreply.github.com>
2021-03-09 21:55:59 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index.php sample</title>
+</head>
+
+<body>
+    <h1>simple-chat-2020</h1>
+    <p>これはトップページのサンプルです。</p>
+</body>
+
+</html>
