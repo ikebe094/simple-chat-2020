806a810d974b3b27cbca7cfa8df71e5f57d1909b
ikebe094 <53401298+ikebe094@users.noreply.github.com>
2021-03-09 21:59:39 +0900

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理画面</title>
+    <script type="text/javascript" src="./js/admin.js"></script>
+</head>
+
+<body>
+    <h1>管理画面</h1>
+    <p></p>
+</body>
+
+</html>
